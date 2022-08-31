a8a58a67a5268d44abb066c00140b424d33e2e0a
talalabdelaal <talalabdelaal2@gmail.com>
2022-08-31 15:04:45 -0500

    test2

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,6 +3,6 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
-		System.out.println("rolly gang rolly gang rolly gang");
+		System.out.println("hunnid bands hunnid bands hunnid bands");
 	}
 }
